814781dd8ec116529db338c9219dabd3afb72a91
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-22 15:54:32 +0200

    - Added structured endpoint definitions

---
+++ src/PlayoffsApi/PlayoffsApi.API/EndpointDefinitions/WeatherEndpointDefinition.cs
@@ -0,0 +1,32 @@
+﻿using PlayoffsApi.API.Configuration.Endpoints;
+
+namespace PlayoffsApi.API.EndpointDefinitions;
+
+public class WeatherEndpointDefinition : IEndpointDefinition
+{
+    public void DefineEndpoints(WebApplication app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+               new WeatherForecast
+               (
+                   DateTime.Now.AddDays(index),
+                   Random.Shared.Next(-20, 55),
+                   summaries[Random.Shared.Next(summaries.Length)]
+               ))
+                .ToArray();
+            return forecast;
+        });
+    }
+
+    public void DefineServices(IServiceCollection services)
+    {
+        // Singletons if needed
+    }
+}
---
+++ src/PlayoffsApi/PlayoffsApi.API/WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿
+// Add services to the container.
+
+
+// Configure the HTTP request pipeline.
+
+
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
