480d3d464f6b421b69cced0e3ee66b2eab3b5fb2
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-24 20:24:51 +0200

    - Cleaned up OrmBenchmark methods

--- src/PlayoffsApi/PlayoffsApi.Tests/Benchmarks/OrmBenchmarks.cs
+++ src/PlayoffsApi/PlayoffsApi.Tests/Benchmarks/OrmBenchmarks.cs
@@ -3,81 +3,42 @@
 namespace PlayoffsApi.Tests.Benchmarks;
 
 [MemoryDiagnoser]
+[MinIterationTime(100.0)]
 public class OrmBenchmarks
 {
     [Benchmark]
     public async Task SelectDapper()
     {
         var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7129/weather/dapper");
 
-        try
-        {
-            var response = await client.GetAsync("https://localhost:7129/weather/dapper");
-
-            response.EnsureSuccessStatusCode();
-            //string responseBody = await response.Content.ReadAsStringAsync();
-        }
-        catch (HttpRequestException e)
-        {
-            //Console.WriteLine("\nException Caught!");
-            //Console.WriteLine("Message :{0} ", e.Message);
-        }
+        //response.EnsureSuccessStatusCode();
     }
 
     [Benchmark]
     public async Task SelectEf()
     {
         var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7129/weather/ef");
 
-        try
-        {
-            var response = await client.GetAsync("https://localhost:7129/weather/ef");
-
-            response.EnsureSuccessStatusCode();
-            //string responseBody = await response.Content.ReadAsStringAsync();
-        }
-        catch (HttpRequestException e)
-        {
-            //Console.WriteLine("\nException Caught!");
-            //Console.WriteLine("Message :{0} ", e.Message);
-        }
+        //response.EnsureSuccessStatusCode();
     }
 
     [Benchmark]
     public async Task SelectEfRaw()
     {
         var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7129/weather/efraw");
 
-        try
-        {
-            var response = await client.GetAsync("https://localhost:7129/weather/efraw");
-
-            response.EnsureSuccessStatusCode();
-            //string responseBody = await response.Content.ReadAsStringAsync();
-        }
-        catch (HttpRequestException e)
-        {
-            //Console.WriteLine("\nException Caught!");
-            //Console.WriteLine("Message :{0} ", e.Message);
-        }
+        //response.EnsureSuccessStatusCode();
     }
 
     [Benchmark]
     public async Task SelectGraphQL()
     {
         var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7129/graphql?query={weather{id,date,temperatureC,temperatureF,summary}}");
 
-        try
-        {
-            var response = await client.GetAsync("https://localhost:7129/graphql?query={weather{id,date,temperatureC,temperatureF,summary}}");
-
-            response.EnsureSuccessStatusCode();
-            //string responseBody = await response.Content.ReadAsStringAsync();
-        }
-        catch (HttpRequestException e)
-        {
-            //Console.WriteLine("\nException Caught!");
-            //Console.WriteLine("Message :{0} ", e.Message);
-        }
+        //response.EnsureSuccessStatusCode();
     }
 }
