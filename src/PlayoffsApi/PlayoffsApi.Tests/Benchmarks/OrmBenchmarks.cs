9a8b3e9eb2ca55dc6cf825cba9b1119e42ea8aeb
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-25 00:44:08 +0200

    - Removed old tests

--- src/PlayoffsApi/PlayoffsApi.Tests/Benchmarks/OrmBenchmarks.cs
+++ src/PlayoffsApi/PlayoffsApi.Tests/Benchmarks/OrmBenchmarks.cs
@@ -7,33 +7,6 @@ namespace PlayoffsApi.Tests.Benchmarks;
 public class OrmBenchmarks
 {
     [Benchmark]
-    public async Task SelectDapper()
-    {
-        var client = new HttpClient();
-        var response = await client.GetAsync("https://localhost:7129/weather/dapper");
-
-        //response.EnsureSuccessStatusCode();
-    }
-
-    [Benchmark]
-    public async Task SelectEf()
-    {
-        var client = new HttpClient();
-        var response = await client.GetAsync("https://localhost:7129/weather/ef");
-
-        //response.EnsureSuccessStatusCode();
-    }
-
-    [Benchmark]
-    public async Task SelectEfRaw()
-    {
-        var client = new HttpClient();
-        var response = await client.GetAsync("https://localhost:7129/weather/efraw");
-
-        //response.EnsureSuccessStatusCode();
-    }
-
-    [Benchmark]
     public async Task SelectGraphQL()
     {
         var client = new HttpClient();
