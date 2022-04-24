37ad808c5ac96a350c1b93549e678b370555e881
Michał Droździk <m.drozdzik97@gmail.com>
2022-04-24 13:41:16 +0200

    - Created Tests project with Dapper vs EF comparison

---
+++ src/PlayoffsApi/PlayoffsApi.Tests/DapperVsEfBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PlayoffsApi.Tests;
+
+[MemoryDiagnoser]
+public class DapperVsEfBenchmark
+{
+    [Benchmark]
+    public async Task SelectDapper()
+    {
+        var client = new HttpClient();
+
+        try
+        {
+            var response = await client.GetAsync("https://localhost:7129/weather/dapper");
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException e)
+        {
+            //Console.WriteLine("\nException Caught!");
+            //Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+
+    [Benchmark]
+    public async Task SelectEf()
+    {
+        var client = new HttpClient();
+
+        try
+        {
+            var response = await client.GetAsync("https://localhost:7129/weather/ef");
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException e)
+        {
+            //Console.WriteLine("\nException Caught!");
+            //Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+}
---
+++ src/PlayoffsApi/PlayoffsApi.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace PlayoffsApi.Tests;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<DapperVsEfBenchmark>();
+        Console.ReadLine();
+    }
+}
